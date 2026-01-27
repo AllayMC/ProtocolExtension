96c42667a6d125fb895378218439405bc4792970
daoge_cmd <3523206925@qq.com>
2026-01-27 10:51:22 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Protocol Extension
+
+A NetEase (China) Minecraft protocol extension library for [CloudburstMC/Protocol](https://github.com/CloudburstMC/Protocol).
+
+## Features
+
+- NetEase client compression support (raw deflate format)
+- NetEase login chain validation with NetEase public key
+
+## Supported Protocols
+
+| Protocol Version | Minecraft Version |
+|------------------|-------------------|
+| v766             | 1.21.50           |
+
+## Installation
+
+### Gradle (Kotlin DSL)
+
+```kotlin
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.allaymc:protocol-extension:0.1.0")
+}
+```
+
+### Maven
+
+```xml
+<dependency>
+    <groupId>org.allaymc</groupId>
+    <artifactId>protocol-extension</artifactId>
+    <version>0.1.0</version>
+</dependency>
+```
+
+## Usage
+
+### Using NetEase Codec
+
+```java
+import org.allaymc.protocol.extension.v766.Bedrock_v766_NetEase;
+import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
+
+BedrockCodec codec = Bedrock_v766_NetEase.CODEC;
+session.setCodec(codec);
+```
+
+### Using NetEase Compression
+
+```java
+import org.allaymc.protocol.extension.NetEaseCompression;
+import org.cloudburstmc.protocol.bedrock.netty.codec.compression.BatchCompression;
+
+BatchCompression compression = new NetEaseCompression();
+session.getPeer().setCompression(new SimpleCompressionStrategy(compression));
+```
+
+### Validating NetEase Login Chain
+
+```java
+import org.allaymc.protocol.extension.NetEaseEncryptionUtils;
+import org.cloudburstmc.protocol.bedrock.util.ChainValidationResult;
+
+ChainValidationResult result = NetEaseEncryptionUtils.validateChain(chainData);
+if (result.signed()) {
+    // Chain is valid and signed by NetEase
+}
+```
+
+### Detecting NetEase Clients
+
+NetEase clients use RakNet protocol version 8:
+
+```java
+int NETEASE_RAKNET_VERSION = 8;
+boolean isNetEaseClient = session.getPeer().getRakVersion() == NETEASE_RAKNET_VERSION;
+```
+
+## License
+
+LGPL 3.0 - see [LICENSE](LICENSE) for details.
+
+## Credits
+
+- [AllayMC](https://github.com/AllayMC/Allay) - Original implementation
+- [CloudburstMC/Protocol](https://github.com/CloudburstMC/Protocol) - Base protocol library
---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+
+plugins {
+    id("java-library")
+    id("com.vanniktech.maven.publish") version "0.34.0"
+}
+
+group = "org.allaymc"
+version = "0.1.0-SNAPSHOT"
+description = "NetEase protocol extension for CloudburstMC/Protocol"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.opencollab.dev/maven-snapshots")
+    maven("https://repo.opencollab.dev/maven-releases")
+}
+
+dependencies {
+    api(group = "org.cloudburstmc.protocol", name = "bedrock-codec", version = "3.0.0.Beta11-SNAPSHOT")
+    api(group = "org.cloudburstmc.protocol", name = "bedrock-connection", version = "3.0.0.Beta11-SNAPSHOT")
+    compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.34")
+    annotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.34")
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        configureEach {
+            options.isFork = true
+        }
+    }
+
+    // We already have sources jar, so no need to build Javadoc, which would cause a lot of warnings
+    withType<Javadoc> {
+        enabled = false
+    }
+
+    withType<Copy> {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configure<MavenPublishBaseExtension> {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(project.group.toString(), "protocol-extension", project.version.toString())
+
+    pom {
+        name.set("Protocol Extension")
+        description.set("NetEase protocol extension for CloudburstMC/Protocol")
+        inceptionYear.set("2026")
+        url.set("https://github.com/AllayMC/ProtocolExtension")
+
+        scm {
+            connection.set("scm:git:git://github.com/AllayMC/ProtocolExtension.git")
+            url.set("https://github.com/AllayMC/ProtocolExtension")
+        }
+
+        licenses {
+            license {
+                name.set("LGPL 3.0")
+                url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+            }
+        }
+
+        developers {
+            developer {
+                name.set("AllayMC Team")
+                organization.set("AllayMC")
+                organizationUrl.set("https://github.com/AllayMC")
+            }
+        }
+    }
+}
